6e1faf216cace27876d4a948346b38b3665651fa
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-22 15:42:29 +0545

    Default structure setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "../styles/globals.css";
+import { Poppins } from "@next/font/google";
+import { Footer, Navbar, Sidebar } from "../components";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className={poppins.className}>
+      <head />
+      <body>
+        <div className="flex flex-col">
+          <Navbar />
+          <div className="flex">
+            <Sidebar />
+            <div className="flex-1 bg-gray-400">{children}</div>
+          </div>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
