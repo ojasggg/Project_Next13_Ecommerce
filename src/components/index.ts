f0696987b3eed3ae4dbabe12f2a143a756f97427
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-23 00:49:45 +0545

    default exporting

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Sidebar } from "./Sidebar";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer";
+export { default as Card } from "./Card";
+export { default as ClientProvider } from "./ClientProvider";
