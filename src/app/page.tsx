34dedc46f2c78709ab33a553e0db575a40b9762f
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-23 15:41:31 +0545

    Main page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,22 @@ const HomePage = () => {
           className="h-[360px] w-full xl:h-[460px]"
         />
       </section>
-      <section>
-        <h2>Offer for you!</h2>
+      <section className="my-20">
+        <h2 className="mb-8 text-2xl font-bold">Offer for you!</h2>
         <div className="grid gap-6 lg:grid-cols-4 xl:grid-cols-5">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+      <section className="my-20">
+        <h2 className="mb-8 text-2xl font-bold">Featured Products</h2>
+        <div className="grid gap-6 lg:grid-cols-4 xl:grid-cols-5">
+          <Card />
+          <Card />
+          <Card />
           <Card />
         </div>
       </section>
