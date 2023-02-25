504901dee40a90635b21a2e855acfc5e5fc3902e
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-25 16:18:12 +0545

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Hero, ProductList } from "../components";
+
+const newArrival = [
+  "Top 20",
+  "Audio & Video",
+  "Laptops & Computers",
+  "Video Cameras",
+];
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main>
+      <Hero />
+      <div className="mt-10">
+        <ProductList heading="Hot New Arrivals" itemCategories={newArrival} />
+      </div>
+      <div className="mt-10">
+        <ProductList heading="Trending Now" itemCategories={newArrival} />
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
