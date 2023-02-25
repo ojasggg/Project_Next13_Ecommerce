79ec557875af6f7059eb815ca803d018cbd7db4e
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-25 23:21:20 +0545

    Redirect root route to /home

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   experimental: {
     appDir: true,
